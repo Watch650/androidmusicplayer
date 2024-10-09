ea8cb6034831a728368c4c1609cd43b22bf7b3c5
LeBaHaiLong <longhaile04@gmail.com>
2024-10-09 23:49:24 +0700

    search page (unfinished)

--- midgroupproject/app/src/main/java/vn/edu/usth/midgroupproject/search/SearchFragment.java
+++ midgroupproject/app/src/main/java/vn/edu/usth/midgroupproject/search/SearchFragment.java
@@ -3,151 +3,124 @@ package vn.edu.usth.midgroupproject.search;
 import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
-import androidx.appcompat.widget.SearchView;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.core.view.MenuProvider;
-import androidx.lifecycle.Lifecycle;
-
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import vn.edu.usth.midgroupproject.R;
 
 public class SearchFragment extends Fragment {
 
+    Toolbar toolbar;
     private RecyclerView rvSongs;
-    private SongAdapter songAdapter;
-    private SearchView searchView;
+    private SongAdapter adapter;
+    private MenuItem menuItem;
+    private SearchView  searchView;
+    List<Song> options;
     private LinearLayout linearLayout;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-        // Initialize RecyclerView and set adapter
+        toolbar = view.findViewById(R.id.searchbar);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setTitle("Artists, songs, podcasts");
+
         rvSongs = view.findViewById(R.id.rv_songs);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext());
-        rvSongs.setLayoutManager(linearLayoutManager);
+        rvSongs.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        songAdapter = new SongAdapter(getListSongs());
-        rvSongs.setAdapter(songAdapter);
-
-        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(requireContext(), LinearLayout.VERTICAL);
-        rvSongs.addItemDecoration(itemDecoration);
+        adapter = new SongAdapter(options);
+        rvSongs.setAdapter(adapter);
 
         linearLayout = view.findViewById(R.id.content_wrapper);
+        linearLayout.setVisibility(View.GONE);
+
 
         return view;
     }
 
-    private List<Song> getListSongs() {
-        List<Song> list = new ArrayList<>();
-        list.add(new Song("Dangerously", "Charlie Puth", R.drawable.img_1));
-        list.add(new Song("Black in Black", "AC/DC", R.drawable.img_2));
-        list.add(new Song("You Give Love a Bad Name", "Jon Davi", R.drawable.img_5));
-        list.add(new Song("Song 4", "Artist 4", R.drawable.img_8));
-        list.add(new Song("Song 5", "Artist 5", R.drawable.img_10));
-        // Add more songs as needed
-        return list;
-    }
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
 
-        // Adding the MenuProvider for handling menu creation and interactions
-        requireActivity().addMenuProvider(new MenuProvider() {
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.searchbar, menu);
+        menuItem = menu.findItem(R.id.searchbar);
+        searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setIconified(true);
+
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onCreateMenu(Menu menu, MenuInflater menuInflater) {
-                // Inflate the menu
-                menuInflater.inflate(R.menu.searchbar, menu);
-
-                // Set up SearchManager and SearchView
-                SearchManager searchManager = (SearchManager) requireActivity().getSystemService(Context.SEARCH_SERVICE);
-                searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-
-                // Configure the search information
-                searchView.setSearchableInfo(searchManager.getSearchableInfo(requireActivity().getComponentName()));
-
-
-                // Implement search functionality
-                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                    @Override
-                    public boolean onQueryTextSubmit(String query) {
-                        songAdapter.getFilter().filter(query);  // Assuming the adapter has a filter method
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onQueryTextChange(String newText) {
-                        songAdapter.getFilter().filter(newText);  // Filter the results in real-time
-                        return false;
-                    }
-                });
-
-                searchView.setOnSearchClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Show RecyclerView and hide content layout on search click
-                        rvSongs.setVisibility(View.VISIBLE);
-                        linearLayout.setVisibility(View.INVISIBLE);
-                    }
-                });
-
-                searchView.setOnCloseListener(new SearchView.OnCloseListener() {
-                    @Override
-                    public boolean onClose() {
-                        // Hide RecyclerView and show content layout when search view is closed
-                        rvSongs.setVisibility(View.INVISIBLE);
-                        linearLayout.setVisibility(View.VISIBLE);
-                        return true;
-                    }
-                });
+            public boolean onQueryTextSubmit(String query) {
+                mysearch(query);
+                return true;
             }
 
             @Override
-            public boolean onMenuItemSelected(MenuItem menuItem) {
-                // Handle menu item selection if needed
-                return false;
+            public boolean onQueryTextChange(String query) {
+                mysearch(query);
+                return true;
             }
-        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
+        });
+        
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void mysearch(String query) {
+        adapter = new SongAdapter(options);
+        adapter.startListening();
+        rvSongs.setAdapter(adapter);
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+    public void onStop(){
+        super.onStop();
+        adapter.stopListening();
     }
 }
--- midgroupproject/app/src/main/java/vn/edu/usth/midgroupproject/search/SongAdapter.java
+++ midgroupproject/app/src/main/java/vn/edu/usth/midgroupproject/search/SongAdapter.java
@@ -1,29 +1,21 @@
 package vn.edu.usth.midgroupproject.search;
 
-import vn.edu.usth.midgroupproject.R;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
+import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
 import java.util.List;
+import vn.edu.usth.midgroupproject.R;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
 
-public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> implements Filterable{
+    private List<Song> songs;
 
-    private List<Song> songList;
-    private List<Song> songListOld;
-
-    public SongAdapter(List<Song> songList){
-        this.songList = songList;
-        this.songListOld = songList;
+    public SongAdapter(List<Song> songs) {
+        this.songs = songs;
     }
 
     @NonNull
@@ -35,67 +27,42 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull SongViewHolder holder, int position) {
-        Song song = songList.get(position);
-        if (song == null) {
-            return;
-        }
-        holder.ImgSong.setImageResource(song.getImage());
-        holder.SongName.setText(song.getSongName());
-        holder.ArtistName.setText(song.getArtistName());
+        Song song = songs.get(position);
+        holder.songName.setText(song.getSongName());
+        holder.songArtist.setText(song.getArtistName());
+        holder.songImage.setImageResource(song.getImage());
     }
 
     @Override
     public int getItemCount() {
-        if (songList != null) {
-            return songList.size();
-        }
-        return 0;
+        return songs.size();
     }
 
-    @Override
-    public Filter getFilter() {
-        return new Filter() {
-            @Override
-            protected FilterResults performFiltering(CharSequence constraint) {
-                String strSearch = constraint.toString();
-                if (strSearch.isEmpty()) {
-                    songList = songListOld;
-                } else {
-                    List<Song> list = new ArrayList<>();
-                    for (Song song : songListOld) {
-                        if (song.getSongName().toLowerCase().contains(strSearch.toLowerCase())) {
-                            list.add(song);
-                        }
-                    }
-
-                    songList = list;
-                }
-
-                FilterResults filterResults = new FilterResults();
-                filterResults.values = songList;
-
-                return filterResults;
-            }
-
-            @Override
-            protected void publishResults(CharSequence constraint, FilterResults results) {
-                songList = (List<Song>) results.values;
-                notifyDataSetChanged();
-            }
-        };
+    public void updateData(List<Song> newSongs) {
+        songs.clear();
+        songs.addAll(newSongs);
+        notifyDataSetChanged();
     }
 
-    public class SongViewHolder extends RecyclerView.ViewHolder{
+    public void startListening() {
+        // If needed, add logic to start observing data changes (e.g., from a database or API)
+        notifyDataSetChanged();
+    }
 
-        private CircleImageView ImgSong;
-        private TextView SongName;
-        private TextView ArtistName;
+    public void stopListening() {
+        // Logic to stop observing data changes, if applicable
+    }
+
+    public static class SongViewHolder extends RecyclerView.ViewHolder {
+        TextView songName;
+        TextView songArtist;
+        ImageView songImage;
 
         public SongViewHolder(@NonNull View itemView) {
             super(itemView);
-            ImgSong = itemView.findViewById(R.id.Songicon);
-            SongName = itemView.findViewById(R.id.song_name);
-            ArtistName = itemView.findViewById(R.id.artist_name);
+            songName = itemView.findViewById(R.id.song_name);
+            songArtist = itemView.findViewById(R.id.artist_name);
+            songImage = itemView.findViewById(R.id.Songicon);
         }
     }
 }
