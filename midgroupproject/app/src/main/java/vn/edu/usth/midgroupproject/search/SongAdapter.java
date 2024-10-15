1915880f95d3e660c1c86974bddca7e56a20a1f1
LeBaHaiLong <longhaile04@gmail.com>
2024-10-15 12:00:45 +0700

    search page (unfinished)

--- midgroupproject/app/src/main/java/vn/edu/usth/midgroupproject/search/SongAdapter.java
+++ midgroupproject/app/src/main/java/vn/edu/usth/midgroupproject/search/SongAdapter.java
@@ -3,66 +3,62 @@ package vn.edu.usth.midgroupproject.search;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import vn.edu.usth.midgroupproject.R;
 
-public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
 
-    private List<Song> songs;
+    private final List<Map<String, String>> searchResults;
+    private final OnResultClickListener onResultClickListener;
 
-    public SongAdapter(List<Song> songs) {
-        this.songs = songs;
+    public interface OnResultClickListener {
+        void onResultClicked(Map<String, String> result);
+    }
+
+    public SongAdapter(List<Map<String, String>> searchResults, OnResultClickListener onResultClickListener) {
+        this.searchResults = searchResults;
+        this.onResultClickListener = onResultClickListener;
     }
 
     @NonNull
     @Override
-    public SongViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.songs_list, parent, false);
-        return new SongViewHolder(view);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull SongViewHolder holder, int position) {
-        Song song = songs.get(position);
-        holder.songName.setText(song.getSongName());
-        holder.songArtist.setText(song.getArtistName());
-        holder.songImage.setImageResource(song.getImage());
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Map<String, String> result = searchResults.get(position);
+        holder.songName.setText(result.get("song"));
+        holder.artistName.setText(result.get("artist"));
+
+        holder.itemView.setOnClickListener(v -> onResultClickListener.onResultClicked(result));
     }
 
     @Override
     public int getItemCount() {
-        return songs.size();
+        return searchResults.size();
     }
 
-    public void updateData(List<Song> newSongs) {
-        songs.clear();
-        songs.addAll(newSongs);
-        notifyDataSetChanged();
-    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView songName;
+        public TextView artistName;
 
-    public void startListening() {
-        // If needed, add logic to start observing data changes (e.g., from a database or API)
-        notifyDataSetChanged();
-    }
-
-    public void stopListening() {
-        // Logic to stop observing data changes, if applicable
-    }
-
-    public static class SongViewHolder extends RecyclerView.ViewHolder {
-        TextView songName;
-        TextView songArtist;
-        ImageView songImage;
-
-        public SongViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
             songName = itemView.findViewById(R.id.song_name);
-            songArtist = itemView.findViewById(R.id.artist_name);
-            songImage = itemView.findViewById(R.id.Songicon);
+            artistName = itemView.findViewById(R.id.artist_name);
         }
     }
 }
